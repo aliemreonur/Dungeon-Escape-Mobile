1ee566545df34d465a47bfb086fce721639c954c
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-05-21 15:41:34 +0300

    Player Setup Finished

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private Animator _swordAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        if(_anim == null)
+        {
+            Debug.LogError("Player animation could not get the animator ");
+        }
+        _swordAnim = transform.GetChild(1).GetComponent<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void Jump(bool jumping)
+    {
+        _anim.SetBool("Jump", jumping);
+    }
+
+    public void Attack()
+    {
+        _anim.SetTrigger("Attack");
+        _swordAnim.SetTrigger("SwordAnim");
+    }
+}
